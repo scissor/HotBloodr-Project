f168a5c02b2629c92a668a6caba82faf3321ccee
Scissor <scissorsmail@gmail.com>
2017-07-13 09:44:30 +0800

    Add ContainerExtension & sublime project.

---
+++ Assets/Plugins/HotBloodr/Scripts/Extensions/ContainerExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static partial class ExtensionMethods
+{
+    public static T CircularNext<T>(this IList<T> list, T now)
+    {
+        var index = list.IndexOf(now);
+        ++index;
+
+        if (index >= list.Count)
+        {
+            return list.First();
+        }
+        return list[index];
+    }
+
+    public static T CircularPrev<T>(this IList<T> list, T now)
+    {
+        var index = list.IndexOf(now);
+        --index;
+
+        if (index < 0)
+        {
+            return list.Last();
+        }
+        return list[index];
+    }
+}
